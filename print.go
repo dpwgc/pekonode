51bbd2c9c24e069ccfb7320c9a5af31e02c4bdca
dpwgc <281148314@qq.com>
2022-12-26 11:39:00 +0800

    log update

--- print.go
+++ print.go
@@ -2,16 +2,15 @@ package pekonode
 
 import (
 	"log"
-	"time"
 )
 
 //打印信息到控制台
 func (nodeList *NodeList) println(a ...interface{}) {
 	//输出错误信息
-	if a[0] == "[Error]:" {
-		log.Println(a[1])
+	if a[0] == "[Error]:" && !nodeList.IsPrint {
+		log.Println(a)
 	}
 	if nodeList.IsPrint {
-		log.Println(time.Now().Format("2006-01-02 15:04:05"), a)
+		log.Println(a)
 	}
 }
